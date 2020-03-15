0ffd28eb5e451e57ce512a9aea4ec7c8756d447a
Tibor Toth <40400971@live.napier.ac.uk>
2020-03-15 17:43:03 +0000

    unit test file added and code coverage initiated

---
+++ src/test/java/com/napier/sem/AppTest.java
@@ -0,0 +1,21 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    static App app;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+    }
+
+    @Test
+    void printCountriesTestNull()
+    {
+        app.largestToSmallestPopulationInCountry(null);
+    }
+}
